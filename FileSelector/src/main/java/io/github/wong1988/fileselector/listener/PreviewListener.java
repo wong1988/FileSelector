878b026f9a89ed621bf0c5bb835de770318bc04d
wong1988 <wong1988@petalmail.com>
2022-06-10 17:03:41 +0800

    Update PreviewListener.java

--- FileSelector/src/main/java/io/github/wong1988/fileselector/listener/PreviewListener.java
+++ FileSelector/src/main/java/io/github/wong1988/fileselector/listener/PreviewListener.java
@@ -2,13 +2,17 @@ package io.github.wong1988.fileselector.listener;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+import io.github.wong1988.fileselector.entity.ImgInfo;
+
 public interface PreviewListener {
 
     void onAlphaChanged(float alpha);
 
     void onTranslationYChanged(float translationY);
 
-    void onClosed();
+    void onClosed(List<ImgInfo> deleteList, List<ImgInfo> result);
 
     void onScrollStateChanged(RecyclerView recyclerView, int newState);
 

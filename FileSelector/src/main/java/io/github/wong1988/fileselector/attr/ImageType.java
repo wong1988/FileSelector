caccc6a08d9f87d9162b680a268677c1421fe721
wong1988 <wong1988@petalmail.com>
2022-06-11 10:51:17 +0800

    Create ImageType.java

---
+++ FileSelector/src/main/java/io/github/wong1988/fileselector/attr/ImageType.java
@@ -0,0 +1,8 @@
+package io.github.wong1988.fileselector.attr;
+
+public enum ImageType {
+    /**
+     * R.mipmap.xxx,https://xxx.x.jpg,/storage/emulated/xxx/xxx/xx.jpg
+     */
+    ImageResource, HTTP, File;
+}

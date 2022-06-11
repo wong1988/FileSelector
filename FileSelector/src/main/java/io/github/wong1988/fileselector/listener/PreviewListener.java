d52857cc0a576e5498d602ef331e650e70ac4ef1
wong1988 <wong1988@petalmail.com>
2022-06-11 10:54:11 +0800

    Update PreviewListener.java

--- FileSelector/src/main/java/io/github/wong1988/fileselector/listener/PreviewListener.java
+++ FileSelector/src/main/java/io/github/wong1988/fileselector/listener/PreviewListener.java
@@ -12,11 +12,17 @@ public interface PreviewListener {
 
     void onTranslationYChanged(float translationY);
 
+    /**
+     * 关闭页面时同时回调删除的图片以及未删除的图片集合
+     */
     void onClosed(List<ImgInfo> deleteList, List<ImgInfo> result);
 
     void onScrollStateChanged(RecyclerView recyclerView, int newState);
 
     void onScrolled(RecyclerView recyclerView, int dx, int dy);
 
+    /**
+     * 当前选中的页面index
+     */
     void onPageSelected(int position);
 }
